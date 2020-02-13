f9ffb2bb6c9e250769c955d1237315bea2229ee2
mlissad000 <54646660+mlissad000@users.noreply.github.com>
2020-02-13 18:19:20 -0500

    bars oriented horizontally

--- tutorial2_quantities_and_amounts/main.js
+++ tutorial2_quantities_and_amounts/main.js
@@ -8,12 +8,12 @@ d3.csv("../data/squirrelActivities.csv", d3.autoType).then(data => {
 
     const xScale = d3
         .scaleLinear()
-        .domain(data.map(d => d.count))
+        .domain([0, d3.max(data, d => d.count)])
         .range([margin.left, width - margin.right]);
 
     const yScale = d3
         .scaleBand()
-        .domain([0, d3.max(data, d => d.activity)])
+        .domain(data.map(d => d.activity))
         .range([height - margin.bottom, margin.top])
         .paddingInner(paddingInner);
 
@@ -34,18 +34,18 @@ d3.csv("../data/squirrelActivities.csv", d3.autoType).then(data => {
         .attr("width", d => width - margin.left - xScale(d.count))
         .attr("height", yScale.bandwidth())
         .attr("fill", "steelblue")
-/*
+
     const text = svg
         .selectAll("text")
         .data(data)
         .join("text")
         .attr("class", "label")
         // this allows us to position the text in the center of the bar
-        .attr("x", d => xScale(d.activity) + (xScale.bandwidth() / 2))
-        .attr("y", d => yScale(d.count))
+        .attr("x", d => xScale(d.count))
+        .attr("y", d => yScale(d.activity) + (yScale.bandwidth() / 2))
         .text(d => d.count)
         .attr("dy", "1.25em");
-*/
+
     svg
         .append("g")
         .attr("class", "axis")
